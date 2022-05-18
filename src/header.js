6d8eef9320f53d9054790155eb29005e068096af
Achint2103 <gusainachint@gmail.com>
2022-05-18 21:39:58 +0530

    Intial commit

---
+++ src/header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+    <nav>
+    <h1>Bistro 657</h1>
+    <ul>
+        <li>Menu</li>
+        <li>About</li>
+        <li>Contact</li>
+        </ul>
+    </nav>
+    </div>
+  )
+}
+
+export default Header
